66a81611e035e721cff0704150ed762b18ff4012
ws311472 <43374494+ws311472@users.noreply.github.com>
2018-10-22 13:06:20 +0100

    Added a headers header file and tidied up the comments a bit

--- PortfolioTask2018/graphic.h
+++ PortfolioTask2018/graphic.h
@@ -1,4 +1,39 @@
 #pragma once
+#include <cstdio>
+#include <iostream>
+//Just because I prefer using elif (plus it's quicker)
+#define elif else if 
+#ifdef _MSC_VER
+#endif
+#define ToUpper(string_data) transform(string_data.begin(), string_data.end(), string_data.begin(), ::toupper);
+using namespace std; 
+
+void gotoxy(int x, int y) //Writes over the existing material in the console. Only works if the one that is on the console next is larger than the one before it.
+{
+	COORD topLeft = { x, y };
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	SetConsoleCursorPosition(console, topLeft);
+}
+
+void clear() //Used to be able to clear the console.
+{
+	COORD topLeft = { 0, 0 };
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO screen;
+	DWORD written;
+
+	GetConsoleScreenBufferInfo(console, &screen);
+	FillConsoleOutputCharacterA(
+		console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+	);
+	FillConsoleOutputAttribute(
+		console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+		screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+	);
+	SetConsoleCursorPosition(console, topLeft);
+}
+
 void loading() //Loading graphics function
 {
 	clear();
@@ -89,4 +124,47 @@ void error()
 		ierror++;
 
 	}
+}
+const string currentDateTime() //Function so that I can call the current time.
+{
+	time_t     now = time(0);
+	struct tm  tstruct;
+	char       buf[80];
+	tstruct = *localtime(&now);
+	strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+
+	return buf;
+}
+
+bool IsYes(std::string in)
+{
+	ToUpper(in);
+	if (in == "Y" || in == "YES") {
+		return true;
+	}
+	return false;
+}
+bool IsNo(std::string in)
+{
+	ToUpper(in);
+	if (in == "N" || in == "NO") {
+		return true;
+	}
+	return false;
+}
+bool IsLoad(std::string in)
+{
+	ToUpper(in);
+	if (in == "L" || in == "LOAD") {
+		return true;
+	}
+	return false;
+}
+bool IsNew(std::string in)
+{
+	ToUpper(in);
+	if (in == "N" || in == "NEW") {
+		return true;
+	}
+	return false;
 }
---
+++ PortfolioTask2018/headers.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <windows.h> 
+#include <ctime>
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <locale>
+#include <fstream>
+#include <chrono>
+#include <ctype.h>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include "graphic.h" //Header file that contains all my functions and defines.
+#include <cstdio>
+
+#pragma comment(lib, "comctl32.lib")
